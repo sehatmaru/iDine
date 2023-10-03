cfcf0feb91dfa285a9c79b5740afb9ddec1c7ee5
sehatmaru <maruzhaky@gmail.com>
2023-10-03 11:24:49 +0700

    init

---
+++ iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by AP on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by AP on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
